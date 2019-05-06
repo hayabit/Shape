1f2fba397bf5f560f1f35294d0322c1fa7341fd5
Hayato <hayabit12@gmail.com>
2019-05-07 00:58:00 +0900

    second commit

--- shaping.nim
+++ shaping.nim
@@ -1,15 +1,9 @@
-# 忌々しい全角「，」や「。」などを自動的に半角とスペースに置き換える
-# 使い方は
-# $ nim c shaping.nim
-# コマンドでコンパイル後に
-# $ ./shaping FILENAME FILENAME
-# というコマンドを打つだけ
-
 import strutils, pegs, unicode, os
 
 if os.paramCount() == 2 :
   var clp = os.commandLineParams()
   var f : File = open($clp[0] , FileMode.fmRead)
+  # ファイルに対して何かしら処理を行った後, defer 内の処理を行う
   defer :
     close(f)
   var rf : string = f.readAll()
